54b1fa3d509411535052697f84bad759f548ab11
Ilyas Karukappadath <ilyasmohamed@hotmail.com>
2017-09-21 21:15:04 -0400

    added clock component

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
           Nav: 'app/components/Nav.jsx',
           Timer: 'app/components/Timer.jsx',
           Countdown: 'app/components/Countdown.jsx',
-          applicationStyles: 'app/styles/app.css'
+          Clock: 'app/components/Clock.jsx',
+           applicationStyles: 'app/styles/app.css'
       },
         extensions: ['', '.js', '.jsx']
     },
