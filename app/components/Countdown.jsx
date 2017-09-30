b05df8b2807f91370074606dc2f657fe3bba3c57
Ilyas Karukappadath <ilyasmohamed@hotmail.com>
2017-09-29 20:54:43 -0400

    added timer

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,11 +4,37 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
     getInitialState: function() {
-        return {count: 0};
+        return {count: 0,
+        countdownStatus: 'stopped'};
     },
+    componentDidUpdate: function(prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch(this.state.countdownStatus) {
+                case 'started':
+                  this.startTimer();
+                  break;
+                case 'stopped':
+                  this.stopTimer();
+                  break;
+            }
+        }
+    },
+    startTimer: function() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count > 0 ? this.state.count - 1 : 0;
+            this.setState({
+                count : newCount
+            });
+        }, 1000);
+    },
+    stopTimer: function() {
+
+    },
+
     handleCountdown: function(seconds) {
         this.setState(
-            {count : seconds}
+            {count : seconds,
+            countdownStatus: 'started'}
         )
     },
    render: function() {
